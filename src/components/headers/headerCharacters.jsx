dd352514e6c0761b4c25cc6216cd57790400e2f1
William Alves <williamcostacardoso94@gmail.com>
2024-01-31 20:54:03 -0300

    feat: passando a função de subir a tela para os componentes

--- src/components/headers/headerCharacters.jsx
+++ src/components/headers/headerCharacters.jsx
@@ -1,12 +1,13 @@
 import { Head, LogoImg, Nav } from "./styles-header";
-import Logo from "../../assets/logo.png";
+import Logo from "../../assets/logotwo.png";
 import { Link } from "react-router-dom";
+import { handleClickLogo } from "../scrollToTop/scrollToTop";
 
 export const HeaderCharacter = () => {
   return (
     <Head>
       <LogoImg>
-        <img src={Logo} alt="" />
+        <img src={Logo} onClick={handleClickLogo} />
       </LogoImg>
       <Nav>
         <Link to="/" className="tag-link">
--- src/components/headers/headerEpisode.jsx
+++ src/components/headers/headerEpisode.jsx
@@ -1,19 +1,20 @@
 import { Head, LogoImg, Nav } from "./styles-header";
-import Logo from "../../assets/logo.png";
+import Logo from "../../assets/logotwo.png";
 import { Link } from "react-router-dom";
+import { handleClickLogo } from "../scrollToTop/scrollToTop";
 
 export const HeaderEpisode = () => {
   return (
     <Head>
       <LogoImg>
-        <img src={Logo} alt="" />
+        <img src={Logo} onClick={handleClickLogo} />
       </LogoImg>
       <Nav>
         <Link to="/characters" className="tag-link">
-          <p className="tag-nav-char">Personagens</p>
+          <a className="tag-nav-char">Personagens</a>
         </Link>
         <Link to="/favorites" className="tag-link">
-          <p className="tag-nav-ep">Episódios Favoritos</p>
+          <a className="tag-nav-ep">Episódios Favoritos</a>
         </Link>
       </Nav>
     </Head>

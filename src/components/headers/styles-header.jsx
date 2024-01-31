2290ae002b737bb92305600e46cf136806a9b0eb
William Alves <williamcostacardoso94@gmail.com>
2024-01-31 00:36:43 -0300

    feat: organização nas tag dos elementos nav do header

--- src/components/headers/styles-header.jsx
+++ src/components/headers/styles-header.jsx
@@ -3,20 +3,11 @@ import styled from "styled-components";
 export const Head = styled.header`
   width: 100%;
   height: 120px;
-  background-color: black;
+  background-color: gray;
   display: flex;
   align-items: center;
   justify-content: space-between;
   border-bottom: 2px solid #25d61f;
-
-  .tag-nav {
-    color: #25d61f;
-    font-family: "Creepster", system-ui;
-    letter-spacing: 2px;
-    text-decoration: none;
-    font-size: 30px;
-    margin-right: 100px;
-  }
 `;
 
 export const LogoImg = styled.header`
@@ -26,5 +17,29 @@ export const LogoImg = styled.header`
     align-items: center;
     display: flex;
     cursor: pointer;
+  }
+`;
+
+export const Nav = styled.div`
+  .tag-nav-char {
+    color: #25d61f;
+    font-family: "Creepster", system-ui;
+    letter-spacing: 2px;
+    text-decoration: none;
+    font-size: 30px;
+    margin-right: 100px;
+  }
+  .tag-nav-ep {
+    color: #25d61f;
+    font-family: "Creepster", system-ui;
+    letter-spacing: 2px;
+    text-decoration: none;
+    font-size: 30px;
+    margin-right: 150px;
+    margin-top: 10px;
+  }
+
+  .tag-link {
+    text-decoration: none;
   }
 `;

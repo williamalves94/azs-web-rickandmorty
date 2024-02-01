8c9357e04be151e1ab8706393a6d3bcea9a19298
William Alves <williamcostacardoso94@gmail.com>
2024-01-31 21:58:09 -0300

    feat: responsividade dos cards

--- src/components/cards/styles-cards.jsx
+++ src/components/cards/styles-cards.jsx
@@ -3,15 +3,15 @@ import styled from "styled-components";
 export const Cards = styled.div`
   //border: 3px solid red;
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  padding: 50px 100px 50px 100px;
-  .char-div {
-    margin-top: 20px;
-    //border: 3px solid #fff;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 3fr));
+  gap: 20px;
+  padding: 5vw;
+  char-div {
     display: grid;
     justify-content: center;
-    padding: 10px 0 10px 0;
+    padding: 10px 0;
   }
+
   .char-and-text {
     border: 2px solid #2bd326;
     cursor: pointer;
@@ -19,10 +19,15 @@ export const Cards = styled.div`
     box-shadow: 10px 10px 15px 0px #00000086;
     margin-bottom: 10px;
   }
+
   .char-image {
-    //border: 3px solid blue;
     border-radius: 6px 6px 0 0;
+    max-width: 100%;
+    min-width: 100%;
+    min-height: 100%;
+    height: auto;
   }
+
   .char-name {
     font-size: 25px;
     margin-left: 10px;
@@ -32,7 +37,8 @@ export const Cards = styled.div`
     text-shadow: 1px 1px 2px black;
   }
 
-  .char-status {
+  .char-status,
+  .char-species {
     font-size: 20px;
     color: black;
     margin: 5px 0 5px 10px;
@@ -41,11 +47,63 @@ export const Cards = styled.div`
     text-shadow: 1px 1px 2px #2bd326;
   }
 
-  .char-species {
-    font-size: 20px;
-    color: black;
-    margin: 5px 0 5px 10px;
-    font-family: "Creepster", system-ui;
-    letter-spacing: 2px;
-    text-shadow: 1px 1px 2px #2bd326;
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(auto-fit, minmax(250px, 3fr));
+    padding: 50px;
+  }
+  @media (max-width: 370px) {
+    grid-template-columns: repeat(auto-fit, minmax(250px, 3fr));
+    padding: 50px;
+  }
 `;
+
+// export const Cards = styled.div`
+//   //border: 3px solid red;
+//   display: grid;
+//   grid-template-columns: repeat(3, 1fr);
+//   padding: 50px 100px 50px 100px;
+//   .char-div {
+//     margin-top: 20px;
+//     //border: 3px solid #fff;
+//     display: grid;
+//     justify-content: center;
+//     padding: 10px 0 10px 0;
+//   }
+//   .char-and-text {
+//     border: 2px solid #2bd326;
+//     cursor: pointer;
+//     border-radius: 6px;
+//     box-shadow: 10px 10px 15px 0px #00000086;
+//     margin-bottom: 10px;
+//   }
+//   .char-image {
+//     //border: 3px solid blue;
+//     border-radius: 6px 6px 0 0;
+//   }
+//   .char-name {
+//     font-size: 25px;
+//     margin-left: 10px;
+//     color: #2bd326;
+//     font-family: "Creepster", system-ui;
+//     letter-spacing: 2px;
+//     text-shadow: 1px 1px 2px black;
+//   }
+
+//   .char-status {
+//     font-size: 20px;
+//     color: black;
+//     margin: 5px 0 5px 10px;
+//     font-family: "Creepster", system-ui;
+//     letter-spacing: 2px;
+//     text-shadow: 1px 1px 2px #2bd326;
+//   }
+
+//   .char-species {
+//     font-size: 20px;
+//     color: black;
+//     margin: 5px 0 5px 10px;
+//     font-family: "Creepster", system-ui;
+//     letter-spacing: 2px;
+//     text-shadow: 1px 1px 2px #2bd326;
+//   }
+// `;

b3006ac3140339a06bc5e34f65f60fb0caed363c
William Alves <williamcostacardoso94@gmail.com>
2024-02-02 13:01:51 -0300

    feat: correção da tela de episódios favoritos

--- src/components/favorites/EpisodesFavorites.jsx
+++ src/components/favorites/EpisodesFavorites.jsx
@@ -7,6 +7,7 @@ import {
   DivEpisodesFav,
   DateAndTotalChar,
   CharDivFav,
+  EmptyList,
 } from "./styles-favorites";
 import { FooterFavorites } from "./Footer-favorites";
 
@@ -24,37 +25,45 @@ export const FavoriteEpisodes = () => {
               Lista de Episódios <br></br>Favoritos
             </p>
           </div>
-          <div></div>
 
-          {favoriteEpisodesData.map((episode) => (
-            <div className="div-all" key={EpisodesMarkFavorite.id}>
-              <div className="ep-name">
-                Episódio {episode.id}: {episode.name}
-              </div>
-              <DateAndTotalChar>
-                <div className="air-date">
-                  Data de Estréia: {episode.air_date}
+          {favoriteEpisodesData.length === 0 ? (
+            <EmptyList>
+              <h1>Lista vazia</h1>
+            </EmptyList>
+          ) : (
+            favoriteEpisodesData.map((episode) => (
+              <div className="div-all" key={EpisodesMarkFavorite.id}>
+                <div className="ep-name">
+                  Episódio {episode.id}: {episode.name}
                 </div>
-                <div className="char-total">
-                  Total de Personagens: {episode.characters.length}
-                </div>
-              </DateAndTotalChar>
-              <CharDivFav className="cards">
-                {episode.characters.map((character) => (
-                  <div className="char-div" key={character.id}>
-                    <div className="char-and-text">
-                      <img className="char-image" src={character.image} />
-                      <h3 className="char-name">{character.name}</h3>
-                      <p className="char-status">Status: {character.status}</p>
-                      <p className="char-species">
-                        Espécie: {character.species}
-                      </p>
-                    </div>
+
+                <DateAndTotalChar>
+                  <div className="air-date">
+                    Data de Estréia: {episode.air_date}
                   </div>
-                ))}
-              </CharDivFav>
-            </div>
-          ))}
+                  <div className="char-total">
+                    Total de Personagens: {episode.characters.length}
+                  </div>
+                </DateAndTotalChar>
+                <CharDivFav className="cards">
+                  {episode.characters.map((character) => (
+                    <div className="char-div" key={character.id}>
+                      <div className="char-and-text">
+                        <img className="char-image" src={character.image} />
+                        <h3 className="char-name">{character.name}</h3>
+                        <p className="char-status">
+                          Status: {character.status}
+                        </p>
+                        <p className="char-species">
+                          Espécie: {character.species}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </CharDivFav>
+              </div>
+            ))
+          )}
         </div>
       </DivEpisodesFav>
       <FooterFavorites />

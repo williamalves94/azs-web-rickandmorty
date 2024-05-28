7c576250661a9c615f778e6dabdeec2cbbae3eb7
William Alves <williamcostacardoso94@gmail.com>
2024-05-28 19:27:48 -0300

    feat: testintg commit branch

---
+++ src/components/characters/footer-characters.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FooterChar, LogoImg } from "./styles-characters";
+import LogoFooter from "../../assets/logotwo.png";
+import { handleClickLogo } from "../scrollToTop/scrollToTop";
+
+export const FooterCharacters = () => {
+  return (
+    <FooterChar>
+      <LogoImg>
+        <img src={LogoFooter} onClick={handleClickLogo} />
+      </LogoImg>
+    </FooterChar>
+  );
+};
